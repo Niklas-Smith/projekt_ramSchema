5f0cd0eebae01fefbf50f6dee209709e14f06f05
Niklas Smith <niklas_smith@hotmail.com>
2025-07-26 09:53:50 +0200

    Gound for projekt typescipt. Made same compotonet (header,footer) and pages for website. also same basic css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
+import { RamschemaComponent } from './ramschema/ramschema.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path:"courses", component:CoursesComponent},
+{path:"ramschema", component:RamschemaComponent},
+{path: "" , redirectTo: "/ramschema", pathMatch: "full"},
+];
---
+++ src/app/ramschema/ramschema.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ramschema',
+  imports: [],
+  templateUrl: './ramschema.component.html',
+  styleUrl: './ramschema.component.css'
+})
+export class RamschemaComponent {
+
+}
