2fef78a6e5e685bb042a940459dbddb78a0ca31d
Niklas Smith <niklas_smith@hotmail.com>
2025-07-28 13:29:50 +0200

    added filter, and removed duplicates from course subject

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,15 +2,21 @@ import { Component } from '@angular/core';
 import { CourseInterface } from '../model/courseInterface';
 import { CourseInfoService } from '../services/courseinfo.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
 courseList: CourseInterface[] = [];
+coursesFilter: CourseInterface[] = [];
+filterInput: string = "";
+
+
+uniqueCourses: CourseInterface[] = [];
 
 constructor(private courseinfoservice: CourseInfoService) {}
 
@@ -19,43 +25,71 @@ constructor(private courseinfoservice: CourseInfoService) {}
     this.courseinfoservice.getCourses().subscribe(data => {
 
       this.courseList = data;
+      this.coursesFilter = data;
+  
+
+     const spottedCourse:string[]= [];
+
+this.uniqueCourses = this.courseList.filter(course=>{
+if(!spottedCourse.includes(course.subject)) {
+spottedCourse.push(course.subject)
+  return true;
+} else {
+
+return false;
+}
+
+})  
+    
+
     }
 
     )
-
-  
-    
+   
   }
 
-sortCode() {
+filterCourses():void {
+this.coursesFilter = this.courseList.filter((course) =>
+course.courseCode.toLowerCase().includes(this.filterInput.toLowerCase()) ||
+course.courseName.toLowerCase().includes(this.filterInput.toLowerCase())
+
+);
+}
+
+
+
+sortCode():void {
 this.courseinfoservice.getCourses().subscribe(data => {
-this.courseList = data;
-this.courseList.sort((a,b) => a.courseCode > b.courseCode ? 1 : -1)
+this.coursesFilter = data;
+this.coursesFilter.sort((a,b) => a.courseCode > b.courseCode ? 1 : -1)
 })
   
 }
-sortName() {
+sortName():void {
 this.courseinfoservice.getCourses().subscribe(data => {
-this.courseList = data;
-this.courseList.sort((a,b) => a.courseName > b.courseName ? 1 : -1)
+this.coursesFilter = data;
+this.coursesFilter.sort((a,b) => a.courseName > b.courseName ? 1 : -1)
 })
   
 }
 
-sortSubject() {
+sortSubject():void {
 this.courseinfoservice.getCourses().subscribe(data => {
-this.courseList = data;
-this.courseList.sort((a,b) => a.subject > b.subject ? 1 : -1)
+this.coursesFilter = data;
+this.coursesFilter.sort((a,b) => a.subject > b.subject ? 1 : -1)
 })
   
 }
-sortPoints() {
+sortPoints():void {
 this.courseinfoservice.getCourses().subscribe(data => {
-this.courseList = data;
-this.courseList.sort((a,b) => a.points > b.points ? 1 : -1)
+this.coursesFilter = data;
+this.coursesFilter.sort((a,b) => a.points > b.points ? 1 : -1)
 })
   
 }
+
+
+
 
 
 }
